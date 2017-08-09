c7a367b3e6dc62c8fc836c56c7c8374b29f4c8f3
bit-user <bit-user@DESKTOP-51BE4EQ>
2017-08-09 13:35:53 +0900

    echo chat

---
+++ src/thread/MultiThreadEx.java
@@ -0,0 +1,38 @@
+package thread;
+
+public class MultiThreadEx {
+	public static void main(String[] args) {
+		/*for (int i = 0; i <= 9; i++) {
+			System.out.print(i);
+			
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}*/
+		
+		/*for(char c = 'a'; c <= 'z'; c++) {
+			System.out.print(c);
+		}*/
+		
+		Thread thread1 = new AlphabetThread();
+		Thread thread2 = new Thread(new DigitThread());
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				for (char c = 'A'; c < 'Z'; c++) {
+					System.out.print(c);
+					try {
+						Thread.sleep(1000);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		}).start();
+		
+		thread1.start();
+		thread2.start();
+	}
+}
